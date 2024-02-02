1589d262022b094c5c934ebc8b0cff90321e0d62
Beka <beknazarnurkulov@gmail.com>
2024-02-02 12:14:03 +0600

    handle errors

--- src/partials/messages/MessagesContent.vue
+++ src/partials/messages/MessagesContent.vue
@@ -3,11 +3,21 @@
       <img v-if="message.user_id" class="rounded-full mr-4" :src="authStore?.userData?.user?.image" width="40" height="40" alt="User 01" />
       <img v-else class="rounded-full mr-4" :src="activeChat?.image" width="40" height="40" alt="User 01" />
       <div v-if="message.type === 'text'">
-        <div
-            class="text-sm rounded-lg border shadow-md mb-1 p-3"
-            :class="message.user_id ? 'bg-indigo-500 text-white rounded-tl-none border-transparent' : 'bg-white dark:bg-slate-800 text-slate-800 dark:text-slate-100 rounded-tl-none border-slate-200 dark:border-slate-7001'"
-        >
-          {{message.text}}
+        <div class="flex">
+          <div
+              class="text-sm rounded-lg border shadow-md mb-1 p-3"
+              :class="message.user_id ? 'bg-indigo-500 text-white rounded-tl-none border-transparent' : 'bg-white dark:bg-slate-800 text-slate-800 dark:text-slate-100 rounded-tl-none border-slate-200 dark:border-slate-7001'"
+          >
+            {{message.text}}
+          </div>
+          <div v-if="message.errors" class="m-4">
+            <div class="flex items-center space-x-2">
+              <!-- Start -->
+              <Tooltip size="md" bg="light">
+                <div class="ml-2 text-xs">{{ message.errors }}</div>
+              </Tooltip>
+            </div>
+          </div>
         </div>
         <MessageStatus :message="message"/>
       </div>
@@ -19,13 +29,32 @@
         <MessageStatus :message="message"/>
       </div>
       <div v-else-if="message.type === 'audio' && message?.attachments[0]">
-        <AudioPlayer :audioPath="message?.attachments[0].path"/>
+        <div class="flex items-center">
+          <AudioPlayer :audioPath="message?.attachments[0].path"/>
+          <div v-if="message.errors" class="m-4">
+            <div class="flex items-center space-x-2">
+              <!-- Start -->
+              <Tooltip size="md" bg="light">
+                <div class="ml-2 text-xs">{{ message.errors }}</div>
+              </Tooltip>
+            </div>
+          </div>
+          <MessageFileLink v-else :message="message" />
+        </div>
         <MessageStatus :message="message"/>
       </div>
       <div v-else-if="message.type === 'video' && message?.attachments[0]">
         <div class="flex items-center">
           <VideoPlayer :videoPath="message?.attachments[0].path"/>
-          <MessageFileLink :message="message" />
+          <div v-if="message.errors" class="m-4">
+            <div class="flex items-center space-x-2">
+              <!-- Start -->
+              <Tooltip size="md" bg="light">
+                <div class="ml-2 text-xs">{{ message.errors }}</div>
+              </Tooltip>
+            </div>
+          </div>
+          <MessageFileLink v-else :message="message" />
         </div>
         <MessageStatus :message="message"/>
       </div>
@@ -43,7 +72,15 @@
             </span>
             <span v-else class="mr-2">Без имени</span>
           </div>
-          <MessageFileLink :message="message" />
+          <div v-if="message.errors" class="m-4">
+            <div class="flex items-center space-x-2">
+              <!-- Start -->
+              <Tooltip size="md" bg="light">
+                <div class="ml-2 text-xs">{{ message.errors }}</div>
+              </Tooltip>
+            </div>
+          </div>
+          <MessageFileLink v-else :message="message" />
         </div>
         <MessageStatus :message="message"/>
       </div>
@@ -58,6 +95,7 @@ import VideoPlayer from "./VideoPlayer.vue";
 import { onMounted  } from "vue";
 import MessageStatus from "./MessageStatus.vue";
 import MessageFileLink from "./MessageFileLink.vue";
+import Tooltip from '../../components/Tooltip.vue'
 
 const { activeChat, markAsRead } = useMessangers();
 const authStore = useAuthStore();
