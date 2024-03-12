4029f4746007a0b0e7514894f8b3ad137c9270eb
Beka <beknazarnurkulov@gmail.com>
2024-03-12 11:51:32 +0600

    whatsapp login

--- src/pages/SocialsPanel.vue
+++ src/pages/SocialsPanel.vue
@@ -122,9 +122,102 @@
                           </footer>
                         </div>
                       </div>
-
+                      <div class="col-span-full xl:col-span-6 2xl:col-span-4 bg-white dark:bg-slate-800 shadow-md rounded-sm border border-slate-200 dark:border-slate-700">
+                        <!-- Card content -->
+                        <div class="flex flex-col h-full p-5">
+                          <div class="grow">
+                            <header class="flex items-center mb-4">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                              </svg>
+                              <h3 class="text-lg text-slate-800 dark:text-slate-100 font-semibold">Facebook</h3>
+                            </header>
+                            <div class="text-sm">Lorem ipsum dolor sit amet eiusmod sed do eiusmod tempor.</div>
+                          </div>
+                          <!-- Card footer -->
+                          <footer class="mt-4">
+                            <div class="flex flex-wrap justify-between items-center">
+                              <!-- Left side -->
+                              <div class="flex space-x-3">
+                                <div class="flex items-center text-slate-400 dark:text-slate-500">
+                                  <svg class="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
+                                    <path d="M14.14 9.585a2.5 2.5 0 00-3.522 3.194c-.845.63-1.87.97-2.924.971a4.979 4.979 0 01-1.113-.135 4.436 4.436 0 01-1.343 1.682 6.91 6.91 0 006.9-1.165 2.5 2.5 0 002-4.547h.002zM10.125 2.188a2.5 2.5 0 10-.4 2.014 5.027 5.027 0 012.723 3.078c.148-.018.297-.028.446-.03a4.5 4.5 0 011.7.334 7.023 7.023 0 00-4.469-5.396zM4.663 10.5a2.49 2.49 0 00-1.932-1.234 4.624 4.624 0 01-.037-.516 4.97 4.97 0 011.348-3.391 4.456 4.456 0 01-.788-2.016A6.989 6.989 0 00.694 8.75c.004.391.04.781.11 1.166a2.5 2.5 0 103.86.584z" />
+                                  </svg>
+                                  <div class="text-sm text-slate-500 dark:text-slate-300">4K+</div>
+                                </div>
+                                <div class="flex items-center text-amber-500">
+                                  <svg class="w-4 h-4 shrink-0 fill-current mr-1.5" viewBox="0 0 16 16">
+                                    <path d="M10 5.934L8 0 6 5.934H0l4.89 3.954L2.968 16 8 12.223 13.032 16 11.11 9.888 16 5.934z" />
+                                  </svg>
+                                  <div class="text-sm text-amber-600">4.7</div>
+                                </div>
+                              </div>
+                              <!-- Right side -->
+                              <button class="btn-sm border-slate-200 dark:border-slate-700 hover:border-slate-300 dark:hover:border-slate-600 shadow-sm flex items-center"
+                                      @click="socialAuthInit('facebook')"
+                              >
+                                <svg class="w-3 h-3 shrink-0 fill-current text-emerald-500 mr-2" viewBox="0 0 12 12">
+                                  <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
+                                </svg>
+                                <span>Добавить</span>
+                              </button>
+                            </div>
+                          </footer>
+                        </div>
+                      </div>
                     </div>
                   </section>
+
+                  <div class="overflow-x-auto">
+                    <table class="table-auto w-full dark:text-slate-300">
+                      <!-- Table header -->
+                      <thead class="text-xs font-semibold uppercase text-slate-500 dark:text-slate-400 bg-slate-50 dark:bg-slate-900/20 border-t border-b border-slate-200 dark:border-slate-700">
+                      <tr>
+                        <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-semibold text-left">Название</div>
+                        </th>
+                        <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-semibold text-left">Соц. сеть</div>
+                        </th>
+                        <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-semibold text-left">Статус</div>
+                        </th>
+                        <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-semibold text-left">Действия</div>
+                        </th>
+                      </tr>
+                      </thead>
+                      <!-- Table body -->
+                      <tbody class="text-sm divide-y divide-slate-200 dark:divide-slate-700">
+                      <tr v-for="account in accounts">
+                        <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-bold text-black">{{ account.title }}</div>
+                        </td>
+                        <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-medium text-black" >{{ account.messenger.name }}</div>
+                        </td>
+                        <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                          <div class="font-medium text-green-400" >Активный</div>
+                        </td>
+<!--                        <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">-->
+<!--                          <div class="space-x-1">-->
+<!--                            <button class="text-rose-500 hover:text-rose-600 rounded-full">-->
+<!--                              <span class="sr-only">Delete</span>-->
+<!--                              <svg class="w-8 h-8 fill-current" viewBox="0 0 32 32">-->
+<!--                                <path d="M13 15h2v6h-2zM17 15h2v6h-2z" />-->
+<!--                                <path d="M20 9c0-.6-.4-1-1-1h-6c-.6 0-1 .4-1 1v2H8v2h1v10c0 .6.4 1 1 1h12c.6 0 1-.4 1-1V13h1v-2h-4V9zm-6 1h4v1h-4v-1zm7 3v9H11v-9h10z" />-->
+<!--                              </svg>-->
+<!--                            </button>-->
+<!--                          </div>-->
+<!--                        </td>-->
+
+                      </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+
                 </div>
 
               </div>
@@ -144,10 +237,12 @@ import {onBeforeUnmount, onMounted, ref} from 'vue'
 import Sidebar from '../partials/Sidebar.vue'
 import Header from '../partials/Header.vue'
 import { useAuthStore } from "../stores/auth.store.js";
+import {useMessangers} from "../utils/messengers.js";
 
 const sidebarOpen = ref(false)
 const baseUrl = `${import.meta.env.VITE_API_URL}/auth/redirect`;
 const authStore = useAuthStore()
+const {accounts} = useMessangers()
 
 function registerSocialAuthEvent(register) {
   if (register)
