6610f6dfdd74ac5cc43596cd8e570a5c2d0bf708
beknazar <bnurkulov@code-pilots.ru>
2024-09-11 13:52:55 +0600

    Скрипты продаж и автоудержание клиентов

--- src/partials/marketing/ScriptList.vue
+++ src/partials/marketing/ScriptList.vue
@@ -8,20 +8,20 @@
 
             <!-- Left: Title -->
             <div class="mb-4 sm:mb-0">
-              <h1 class="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold">Триггеры ✨</h1>
+              <h1 class="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold">Скрипты продаж ✨</h1>
             </div>
 
             <!-- Right: Actions  -->
             <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
 
               <!-- Add customer button -->
-              <router-link to="/marketing/autoresponder">
+              <router-link to="/marketing/scripts/create">
                 <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white">
                   <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                     <path
                         d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z"/>
                   </svg>
-                  <span class="hidden xs:block ml-2">Добавить триггер</span>
+                  <span class="hidden xs:block ml-2">Добавить скрипт</span>
                 </button>
               </router-link>
 
