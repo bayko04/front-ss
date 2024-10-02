bf7bca0d02935eeabb4f324bec6c096040d34552
Rahat <kubatbekovdev@gmail.com>
2024-10-02 14:47:05 +0600

    fix ci/cd

--- src/pages/SocialsPanel.vue
+++ src/pages/SocialsPanel.vue
@@ -359,7 +359,7 @@ const telegramBotToken = ref('')
 const deleting = ref(false)
 const deletingAccount = ref(null)
 
-console.log('testrk')
+console.log('testrk2')
 function deleteAccount(account) {
   deleting.value = true
   deletingAccount.value = account
